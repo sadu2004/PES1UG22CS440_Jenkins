749a0f333556555a7deb4e12a0950ce1cecdd61d
Prasad Ishwar <143942877+sadu2004@users.noreply.github.com>
2025-03-25 12:40:55 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout<<"THis is the main file"<<endl;
+  cout<<"This is newly created"<<endl;
+  cout<<"SRN:PES1UG22CS440"<<endl;
+  cout<<this is an error<<endl//error line
+  return 0;
+}
