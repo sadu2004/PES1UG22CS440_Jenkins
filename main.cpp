c38eab548c2789c2f9983fda1dfbf7ce7963bf69
Prasad Ishwar <143942877+sadu2004@users.noreply.github.com>
2025-03-25 12:51:10 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,5 @@ int main(void) {
   cout<<"THis is the main file"<<endl;
   cout<<"This is newly created"<<endl;
   cout<<"SRN:PES1UG22CS440"<<endl;
-  cout<<this is an error<<endl//error line
   return 0;
 }
